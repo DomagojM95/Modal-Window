b91dcc5e7b679d62c3ec93d49d221319fde412dc
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2022-10-26 10:41:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+////////////////////////////// projestc 2# Modal Window //////////////////////////////
+const modal = document.querySelector('.moadl');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; 1 < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
+
+////////////////////////////////////////////////////////////////////////
