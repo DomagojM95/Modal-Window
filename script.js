449e959ab3324d3a66bb141a0cd919459a98055d
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2022-11-01 20:53:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for (let i = 0; 1 < btnsOpenModal.length; i++)
   console.log(btnsOpenModal[i].textContent);
 
 ////////////////////////////////////////////////////////////////////////
-*/
+
 
 /////////////////////// woeking with classe ////////////////////////
 
@@ -35,3 +35,37 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+*/
+
+///////////////////////// handlling click event ////////////////////////
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
